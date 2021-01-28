9d3affb6fa92aae1f0c91a164f32b43a73f742b3
Fredrik Johansson <jeanfredrik@gmail.com>
2021-01-28 16:55:25 +0100

    feat: add `getQuery` export

---
+++ getQuery.js
@@ -0,0 +1,13 @@
+const gql = require("graphql-tag");
+
+module.exports = function getQuery(query) {
+  if (typeof query === "object" && query.definitions) {
+    return query;
+  } else if (typeof query === "string") {
+    return gql(query);
+  } else if (typeof query === "object" && query.source) {
+    return gql(query.source);
+  } else {
+    throw new Error("Could not parse query: " + query);
+  }
+};

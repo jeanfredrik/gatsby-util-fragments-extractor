1306864e1f12ff93949782587e8f2225e8286cbf
Fredrik Johansson <jeanfredrik@gmail.com>
2021-01-28 16:56:04 +0100

    feat: allow ast as argument to `extractFragmentName`

--- extractFragmentName.js
+++ extractFragmentName.js
@@ -1,11 +1,19 @@
+const getQuery = require("./getQuery");
+
 /**
  * Returns the name of the defined fragment in the GrapgQL string
- * @param {string} fragment A GrapgQL string containing a fragment definition
+ * @param {string|object} fragment A GrapgQL string or AST containing a fragment definition
  * @returns {string} The name of the fragment
  */
 const extractFragmentName = (fragment) => {
-  let [, name] = fragment.match(/^\s*fragment\s+(\w+)\s+on\s/m) || [];
-  return name;
+  fragment = getQuery(fragment);
+  if (fragment.kind !== "Document") {
+    return null;
+  }
+  if (fragment.definitions[0].kind !== "FragmentDefinition") {
+    return null;
+  }
+  return fragment.definitions[0].name.value;
 };
 
 module.exports = extractFragmentName;

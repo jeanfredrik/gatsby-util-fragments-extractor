198043882b037531e4fbde1d28815c7aa08e4158
Fredrik Johansson <jeanfredrik@gmail.com>
2021-06-01 22:02:49 +0200

    fix: incompatible require of `graphql-tag`

--- collectGraphQLFragments.test.js
+++ collectGraphQLFragments.test.js
@@ -4,19 +4,21 @@ describe("collectGraphQLFragments", () => {
   it("parses files in dir and returns fragments", async () => {
     const dir = __dirname + "/test-data/fragments";
     let fragments = await collectGraphQLFragments(dir);
-    expect(fragments).toEqual([
-      `fragment MyFirstFragment on MyType {
+    expect(fragments.sort()).toEqual(
+      [
+        `fragment MyFirstFragment on MyType {
     field1
     field2
   }`,
-      `fragment MySecondFragment on MyType {
+        `fragment MySecondFragment on MyType {
     field1
     field2
   }`,
-      `fragment MyThirdFragment on MyType {
+        `fragment MyThirdFragment on MyType {
     field1
     field2
   }`,
-    ]);
+      ].sort(),
+    );
   });
 });
--- getQuery.js
+++ getQuery.js
@@ -1,4 +1,4 @@
-const gql = require("graphql-tag");
+const gql = require("graphql-tag").default;
 
 module.exports = function getQuery(query) {
   if (typeof query === "object" && query.definitions) {
